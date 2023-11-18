1645ff092babe16b4c29f40a35c9dd0080733e42
Sydney <sydneyrpenix@students.abtech.edu>
2023-11-17 21:47:18 -0500

    Create form in edit.php

--- phase04/public/salamanders/edit.php
+++ phase04/public/salamanders/edit.php
@@ -2,7 +2,24 @@
 
 require_once('../../private/initialize.php');
 include(SHARED_PATH . '/salamander-header.php'); 
+$id = $_GET['id'];
 
-echo "<h1>Stub for Edit Salamander</h1>";
+$salamander = find_salamander_by_id($id);
+<div id="content">
+ <h1>Edit Salamander</h1>
+    <form action="<?php echo url_for('salamanders/edit.php?id='. h(u($id))); ?>" method="post">
+        <label for="name">
+        <input type="text" name="name" value="<?php echo h($salamander['name']); ?>"><br>
+
+        <label for="habitat">
+        <input type="text" name="habitat" value="<?php echo h($salamander['habitat']); ?>"><br>
+
+        <label for="description">
+        <input type="text" name="description" value="<?php echo h($salamander['description']); ?>"><br>
+
+        <input type="submit" value="Edit salamander">
+
+    </form>
+</div>
 
 include(SHARED_PATH . '/salamander-footer.php'); ?>
