311d9289cef76b4db53604968d531d19067314f9
Sydney <sydneyrpenix@students.abtech.edu>
2023-11-17 19:09:15 -0500

    Create page for create.php

--- phase04/public/salamanders/create.php
+++ phase04/public/salamanders/create.php
@@ -3,7 +3,20 @@
 require_once('../../private/initialize.php');
 include(SHARED_PATH . '/salamander-header.php'); 
 
-echo "<h1>Stub for Create Salamander</h1>";
+if(is_post_request()){
+
+    $id = $_POST['id'] ?? '';
+    $name = $_POST['name'] ?? '';
+    $habitat = $_POST['habitat'] ?? '';
+    $description = $_POST['description'] ?? '';
+
+    echo "Salamander Form Parameters<br>"
+    echo "Salamander name: " . $name . "<br>";
+    echo "Salamander habitat: " . $habitat . "<br>";
+    echo "Salamander description: " . $description . "<br>";
+} else {
+    redirect_to(url_for('salamanders/index.php'));
+}
 
 include(SHARED_PATH . '/salamander-footer.php'); 
 ?>
