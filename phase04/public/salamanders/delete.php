0bd952c6a2c6a2650ba66a76204534fb901da153
Sydney <sydneyrpenix@students.abtech.edu>
2023-11-17 23:11:49 -0500

    Add query to delete.php

--- phase04/public/salamanders/delete.php
+++ phase04/public/salamanders/delete.php
@@ -1,4 +1,5 @@
 <?php require_once('../../private/initialize.php'); 
+
 include(SHARED_PATH . '/salamander-header.php');
 if(!isset($_GET['id'])) {
     redirect_to(url_for('salamanders/index.php'));
@@ -8,7 +9,21 @@ if(!isset($_GET['id'])) {
   $salamander = find_salamander_by_id($id);
   
   if(is_post_request()) {
-  
+      $sql = "DELETE FROM salamander ";
+      $sql .= "WHERE id='" . $id . "' ";
+      $sql .= "LIMIT 1";
+
+      $result = mysqli_query($db, $sql);
+
+      //FOR DELETE, result is true/false
+      if($result){
+        redirect_to(url_for('salamanders/index.php'));
+      } else {
+        //DELETE failed
+        echo mysqli_error($db);
+        db_disconnect($db);
+        exit;
+      }
   }
   
   $pageTitle = 'Delete Salamander'; ?>
@@ -19,7 +34,7 @@ if(!isset($_GET['id'])) {
       <p><?php echo h($salamander['name']); ?></p>
   
       <form action="<?php echo url_for('salamanders/delete.php?id=' . h(u($salamander['id']))); ?>" method="post">
-          <input type="submit" name="commit" value="Delete Salamander" />
+          <input type="submit" name="commit" value="Delete Salamander">
       </form>
 
 <?php include(SHARED_PATH . '/salamander-footer.php'); ?>
