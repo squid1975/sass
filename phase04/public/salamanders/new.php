ab6dd9d79fc9ce90c1f1a6bbc288ac4b8bde485d
Sydney <sydneyrpenix@students.abtech.edu>
2023-11-17 19:17:55 -0500

    Create form for new.php

--- phase04/public/salamanders/new.php
+++ phase04/public/salamanders/new.php
@@ -1,5 +1,23 @@
 <?php require_once('../../private/initialize.php'); 
 include(SHARED_PATH . '/salamander-header.php');
-echo "<h1>Stub for New Salamander</h1>";
+
+<div id="content">
+    <div class="new">
+        <h1>Create Salamander</h1>
+
+        <form action="<?php echo url_for('salamanders/create.php'); ?>" method="post">
+            <label for="name">Name:</label>
+            <input type="text" name="name" value=""><br>
+            <label for="habitat">Habitat:</label>
+            <input type="text" name="habitat" value=""><br>
+            <label for="description">Description</label>
+            <input type="text" name="description" value=""></br>
+
+            <input type="submit" value="Create salamander">
+           
+        </form>
+    </div>
+    <a href="<?php echo url_for('salamanders/index.php'); ?>">&laquo; Back to Salamanders</a>
+</div>
 
 include(SHARED_PATH . '/salamander-footer.php'); ?>
