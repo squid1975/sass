811fa9eadd8166dfbe9126114722d0d1214839f6
Sydney <sydneyrpenix@students.abtech.edu>
2023-11-21 22:12:40 -0500

    Final commit

--- phase04/private/db_credentials.php
+++ phase04/private/db_credentials.php
@@ -5,8 +5,4 @@ define("DB_USER", "");
 define("DB_PASS", "");
 define("DB_NAME", "");
 
-//a2
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "charli12_doall");
-// define("DB_PASS", "ErYje7HV2o2Lqp7B9gx");
-// define("DB_NAME", "charli12_salamanders");
+?>
